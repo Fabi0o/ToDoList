7db900e25618d8c40b146768c6b3181e04b2f0ca
Fabi0o <marfab32@gmail.com>
2022-04-21 19:29:36 +0200

    creat domelements file to store all crucial dom elements

---
+++ src/domElements.js
@@ -0,0 +1,5 @@
+export const projectNameInput = document.querySelector("#projectNameInput");
+export const descriptionInput = document.querySelector("#descriptionInput");
+export const dueDateInput = document.querySelector("#dueDateInput");
+export const toDoList = document.querySelector("#listContent");
+export const projectsList = document.querySelector("#projects");
